e4ebf06b4363ad99dcc19690b8d296751a656787
VishnuRam16 <vrm6007@gmail.com>
2021-05-20 16:04:59 +0530

    Added all Scripts and Assets to repository

---
+++ Space Shooter Game/Space Shooter/Assets/Scripts/EnemyDam.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDam : MonoBehaviour
+{
+    [Header("Enemy Stats")]
+    [SerializeField] float health = 100;
+    [SerializeField] int scoreValue = 150;
+
+    [Header("Shooting")]
+    float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxtimeBetweenShots = 3f;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] GameObject projectile;
+
+    [Header("Sound Effects")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] [Range(0,1)] float deathSFXVolume = 0.75f;
+    [SerializeField] AudioClip enemyLaserSFX;
+    [SerializeField] [Range(0, 1)] float enemyLaserSFXVolume = 0.75f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxtimeBetweenShots);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxtimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(projectile,
+                   transform.position,
+                   Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        AudioSource.PlayClipAtPoint(enemyLaserSFX, projectile.transform.position, enemyLaserSFXVolume);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer)
+        {
+            return;
+        }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSFXVolume);
+    }
+}
---
+++ Space Shooter Game/Space Shooter/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave config")]
+
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 1.5f;
+    [SerializeField] float spwanRandomFactor = 0.3f;
+    [SerializeField] int noOfEnemies = 5;
+    [SerializeField] float moveSpeed = 1.5f;
+
+    public GameObject GetEnemyPrefab()
+    {
+        return enemyPrefab;
+    }
+
+    public List<Transform> GetWayPoints()
+    {
+        var waveWayPoints = new List<Transform>();
+        foreach (Transform child in pathPrefab.transform)
+        {
+            waveWayPoints.Add(child);
+        }
+
+        return waveWayPoints;
+    }
+
+    public float GetTimeBetweenSpawns()
+    {
+        return timeBetweenSpawns;
+    }
+
+    public float GetSpawnRandomFactor()
+    {
+        return spwanRandomFactor;
+    }
+
+    public int GetNoOfEnemies()
+    {
+        return noOfEnemies;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
